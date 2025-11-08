1d1182bb21b6ba6eadd0f3a67ef45dd464b466be
GeraldOpitz <geraldopitz@gmail.com>
2025-11-08 17:46:56 -0300

    FUM 49 Feature: Implement configuration of networking structure

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,6 +3,8 @@ module "flask_db" {
   name                = "Flask-DB"
   ami_id              = "ami-0c02fb55956c7d316"
   instance_type       = "t3.micro"
+  subnet_id           = module.networking.private_subnet_1_id
+  security_group_id   = module.networking.flask_db_sg_id
   tags = {
     Environment = "dev"
     Role        = "Database"
@@ -14,8 +16,22 @@ module "flask_app" {
   name                = "Flask-App-User-Manager"
   ami_id              = "ami-0c02fb55956c7d316"
   instance_type       = "t3.micro"
+  subnet_id           = module.networking.public_subnet_1_id
+  security_group_id   = module.networking.flask_app_sg_id
   tags = {
     Environment = "dev"
     Role        = "Application"
   }
+}
+
+module "networking" {
+  source       = "../../modules/networking"
+  project_name = "FUM"
+  vpc_cidr     = "10.0.0.0/16"
+
+  subnet_cidr_1 = "10.0.1.0/24"
+  subnet_cidr_2 = "10.0.2.0/24"
+
+  app_port = 5000
+  db_port  = 5432
 }
---
+++ modules/networking/main.tf
@@ -0,0 +1,136 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr_1
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr_2
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_security_group" "flask_app_sg" {
+  name        = "${var.project_name}-app-sg"
+  description = "Port access ${var.app_port}"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Flask App"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-sg"
+  }
+}
+
+resource "aws_security_group" "flask_db_sg" {
+  name        = "${var.project_name}-db-sg"
+  description = "Internal access to PostgreSQL"
+  vpc_id = aws_vpc.main.id
+
+
+  ingress {
+    description = "PostgreSQL"
+    from_port   = var.db_port
+    to_port     = var.db_port
+    protocol    = "tcp"
+    cidr_blocks = [var.subnet_cidr]
+  }
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-db-sg"
+  }
+}
